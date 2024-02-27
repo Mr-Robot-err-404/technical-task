35de9064897723d10534aa9466d955f5d945f802
Harold <harrisonzimbo777@gmail.com>
2024-02-27 15:00:46 +0200

    valid address

--- database/repositories/address/get.ts
+++ database/repositories/address/get.ts
@@ -4,7 +4,6 @@ export async function getValidAddress() {
   return await db
     .selectFrom('address')
     .where('address.address', '!=', 'ANONYMOUS')
-    .where(eb => eb('city_id', '=', 300).or('city_id', '=', 576))
     .select('address.address_id')
     .executeTakeFirst()
 }
